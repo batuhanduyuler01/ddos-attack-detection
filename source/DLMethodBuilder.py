6c7dae05ce2e3e23a64ce46f50f2b2a0a7ff328c
batuhanduyuler01 <duyuler15@itu.edu.tr>
2022-03-29 23:58:38 +0300

    DL Method builder is implemented. input reshaping will be added
    Signed-off-by: batuhanduyuler01 <duyuler15@itu.edu.tr>

--- source/DLMethodBuilder.py
+++ source/DLMethodBuilder.py
@@ -6,6 +6,9 @@ from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, Flatten, ZeroPadding1D, Dense
 
 
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 class DLMethodBuilder(MethodBuilder):
     def __init__(self, dataframe: pd.DataFrame) -> None:
         MethodBuilder.__init__(self, dataframe)
@@ -22,5 +25,15 @@ class DLMethodBuilder(MethodBuilder):
         self.__model.add(Dense(1, activation="sigmoid"))
         self.__model.summary()
 
-    def start_to_compile(optimizer: str):
-        pass
+    def start_to_compile(self, optimizer: str = 'SGD', loss: str = 'mean_absolute_error', metrics: str = 'accuracy', epochNum: int =20, batchSize : int = 10) -> None:
+        
+        startingTime = time.time()
+
+        if (False == MethodBuilder.is_data_splitted(self)):
+            MethodBuilder.split_data(self)
+        
+        self.__model.compile(loss= loss, optimizer= optimizer, metrics = [metrics])
+        self.__model.fit(self.xTrain.values, self.yTrain.values, epochs=epochNum, batch_size=batchSize, validation_data=(self.xTest.values, self.yTest.values))
+
+        endTime = time.time()
+        print(f'1D CNN Results calculated in: {endTime - startingTime} s')
